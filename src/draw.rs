e2711b96e8416a4145cde194781ee8fcae5683e1
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2022-12-25 03:07:29 +0800

    finish main fn & fix some err

--- src/draw.rs
+++ src/draw.rs
@@ -1,5 +1,5 @@
 use piston_window::{rectangle, Context, G2d};
-use piston_window::types::{Color, Width};
+use piston_window::types::Color;
 
 const BLOCK_SIZE: f64 = 25.0;
 
@@ -27,7 +27,7 @@ pub fn draw_rectangle(
   color: Color,
   x: i32,
   y: i32,
-  Width: i32,
+  width: i32,
   height: i32,
   con: &Context,
   g: &mut G2d,
@@ -40,7 +40,7 @@ pub fn draw_rectangle(
       [
         x,
         y,
-        BLOCK_SIZE * (Width as f64),
+        BLOCK_SIZE * (width as f64),
         BLOCK_SIZE * (height as f64),
       ], 
       con.transform,
